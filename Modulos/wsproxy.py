344e5764fca8e5a6191e9835cbd5e476e8acb482
diego <145928215+vpsvip7@users.noreply.github.com>
2024-08-10 10:54:08 -0300

    Actualizar wsproxy.py

--- Modulos/wsproxy.py
+++ Modulos/wsproxy.py
@@ -14,7 +14,7 @@ MSG = ''
 COR = '<font color="null">'
 FTAG = '</font>'
 DEFAULT_HOST = "127.0.0.1:22"
-RESPONSE = "HTTP/1.1 101 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
+RESPONSE = "HTTP/1.1 101 Web Socket Protocol Handshake\r\nConnection: Upgrade\r\nUpgrade: websocket\r\n\r\nHTTP/1.1 200 Connection Established\r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
